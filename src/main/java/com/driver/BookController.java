5b080de62d9e6e667e6aa83ce6f035f0766d15dd
Aditya kumar <adityakumarji123.ak@gmail.com>
2022-11-30 23:07:07 +0530

    updated commit

--- src/main/java/com/driver/BookController.java
+++ src/main/java/com/driver/BookController.java
@@ -13,17 +13,15 @@ public class BookController {
     @Autowired
     BookService bookService;
 
-    private int count;
-    public BookController() {
-        this.count=0;
-    }
+    private int count=1;
+
 
 
 
     @PostMapping("/create-book")
     public ResponseEntity<Book> createBook(@RequestBody Book book)
     {
-        book.setId(count++);
+        book.setId(count);
 
         bookService.createBook(book);
         return new ResponseEntity<>(book, HttpStatus.CREATED);
